99e146551983561a66c524c565cb94271b6c2a38
arulh <arul.howard@telus.net>
2024-03-15 15:36:59 -0700

    added the continuous read thread into the Scale class

--- src/usb_devices/scale.py
+++ src/usb_devices/scale.py
@@ -1,9 +1,11 @@
 import usb.core
+from threading import Thread
 import sys
+import time
 
 class Scale:
     """
-    Represents all scales.
+    Represents the Dymo M25 scale.
     """
 
     def __init__(self, vid: hex, pid: hex) -> None | ValueError:
@@ -14,7 +16,7 @@ class Scale:
             vid: Vendor ID of USB device
             pid: Product ID of USB device
         """
-
+        self.weight = None
         self.dev = usb.core.find(idVendor=vid, idProduct=pid)
 
         if self.dev is None:
@@ -23,20 +25,49 @@ class Scale:
         endpoint = self.dev[0][(0,0)][0]
         self.ep_address = endpoint.bEndpointAddress
         self.ep_packet_size = endpoint.wMaxPacketSize
-        
-    def read_weight(self) -> int:
+        self.thread = Thread(target=self.__continuous_read)
+        self.thread.daemon = True
+
+    def start_thread(self) -> None:
         """
-        Returns the weight read by the scale.
+        Start the continuous read thread
         """
 
-        if self.dev.is_kernel_driver_active(0):
-            self.dev.detach_kernel_driver(0)
+        self.thread.start()
+    
+    def get_weight(self) -> int:
+        """
+        Returns weight updated by continuous read thread.
+        """
 
-        try:
-            data = self.dev.read(self.ep_address, self.ep_packet_size)
-        except:
-            sys.exit("Data not read")
+        while True:
+            if (self.weight is not None):
+                return self.weight
+    
+    def __continuous_read(self) -> None:
+        """
+        Continuously updates weight attribute.
+        """
 
-        weight = data[4] + (256 * data[5])
+        while True:
+            if self.dev.is_kernel_driver_active(0):
+                self.dev.detach_kernel_driver(0)
 
-        return weight
+            try:
+                data = self.dev.read(self.ep_address, self.ep_packet_size)
+            except:
+                sys.exit("Data not read")
+
+            self.weight = data[4] + (256 * data[5])
+
+if __name__ == "__main__":
+    # example usage of Scale class
+    scale = Scale(0x0922, 0x8003)
+    scale.thread.start()
+
+    try:
+        while True:
+            print(f"Weight: {scale.get_weight()} g")
+            time.sleep(5)
+    except KeyboardInterrupt:
+        print("Program terminated")
