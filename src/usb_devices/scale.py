c1009bc0855b49ef0500bf1e80a3ff05c2d038a9
arulh <arul.howard@telus.net>
2024-03-15 16:59:47 -0700

    fixed USB endpoint buffer queue problem

--- src/usb_devices/scale.py
+++ src/usb_devices/scale.py
@@ -1,5 +1,4 @@
 import usb.core
-from threading import Thread
 import sys
 import time
 
@@ -16,7 +15,7 @@ class Scale:
             vid: Vendor ID of USB device
             pid: Product ID of USB device
         """
-        self.weight = None
+
         self.dev = usb.core.find(idVendor=vid, idProduct=pid)
 
         if self.dev is None:
@@ -25,49 +24,31 @@ class Scale:
         endpoint = self.dev[0][(0,0)][0]
         self.ep_address = endpoint.bEndpointAddress
         self.ep_packet_size = endpoint.wMaxPacketSize
-        self.thread = Thread(target=self.__continuous_read)
-        self.thread.daemon = True
-
-    def start_thread(self) -> None:
-        """
-        Start the continuous read thread
-        """
-
-        self.thread.start()
     
     def get_weight(self) -> int:
         """
         Returns weight updated by continuous read thread.
         """
 
-        while True:
-            if (self.weight is not None):
-                return self.weight
-    
-    def __continuous_read(self) -> None:
-        """
-        Continuously updates weight attribute.
-        """
+        if self.dev.is_kernel_driver_active(0):
+            self.dev.detach_kernel_driver(0)
 
-        while True:
-            if self.dev.is_kernel_driver_active(0):
-                self.dev.detach_kernel_driver(0)
+        try:
+            self.dev.reset()
+            data = self.dev.read(self.ep_address, self.ep_packet_size)
+        except:
+            sys.exit("Data not read")
 
-            try:
-                data = self.dev.read(self.ep_address, self.ep_packet_size)
-            except:
-                sys.exit("Data not read")
+        return data[4] + (256 * data[5])
 
-            self.weight = data[4] + (256 * data[5])
 
 if __name__ == "__main__":
     # example usage of Scale class
     scale = Scale(0x0922, 0x8003)
-    scale.thread.start()
 
     try:
         while True:
             print(f"Weight: {scale.get_weight()} g")
-            time.sleep(5)
+            time.sleep(1)
     except KeyboardInterrupt:
         print("Program terminated")
