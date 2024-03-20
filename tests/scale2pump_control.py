89e2167cd6615e3ade0cd81e8fba54b0f2533e64
arulh <arul.howard@telus.net>
2024-03-20 12:11:46 -0700

    updated scale2pump_control

--- tests/scale2pump_control.py
+++ tests/scale2pump_control.py
@@ -13,19 +13,56 @@ import serial
 import time
 import polynomialFunc as pf
 
+class Scale:
+    """
+    Represents the Dymo M25 scale.
+    """
+
+    def __init__(self, vid: hex, pid: hex) -> None | ValueError:
+        """
+        Locates scale device and initializes attributes.
+        
+        Paramaters:
+            vid: Vendor ID of USB device
+            pid: Product ID of USB device
+        """
+
+        self.name = f"scale_{pid}"
+        self.dev = usb.core.find(idVendor=vid, idProduct=pid)
+
+        if self.dev is None:
+            raise ValueError('Device not found')
+        
+        endpoint = self.dev[0][(0,0)][0]
+        self.ep_address = endpoint.bEndpointAddress
+        self.ep_packet_size = endpoint.wMaxPacketSize
+    
+    def get_weight(self) -> int:
+        """
+        Returns weight updated by continuous read thread.
+        """
+
+        if self.dev.is_kernel_driver_active(0):
+            self.dev.detach_kernel_driver(0)
+
+        try:
+            self.dev.reset()
+            data = self.dev.read(self.ep_address, self.ep_packet_size)
+        except:
+            sys.exit("Data not read")
+
+        return data[4] + (256 * data[5])
+
 interval = 60
 
 m_0 = 10000
 
-SCALE_DATA = None
-
 arduino = serial.Serial(port='/dev/ttyACM0', baudrate=57600, timeout=1)
 
+scale = Scale(0x0922, 0x8003)
+
 def get_current_weight():
-    while True:
-        if SCALE_DATA is not None:
-            return SCALE_DATA
-        time.sleep(1)
+    return scale.get_weight()
 
 def extract_specific_cells(csv_path, start_row, end_row, col):
     with open(csv_path, 'r') as file:
@@ -64,7 +101,9 @@ def loop():
         print(f"Elapsed time: {elapsed_time:.2f}s, Current weight: {current_weight}, Expected weight: {expected_weight:.2f}")
         add_to_csv([current_weight, round(expected_weight, 2)])
 
-        if current_weight >= expected_weight:
+        if current_weight < 50:
+            arduino.write('2'.encode())
+        elif current_weight >= expected_weight:
             control_pump(True)  # Turn on the pump
         elif current_weight < expected_weight:
             control_pump(False)  # Turn off the pump
@@ -76,46 +115,6 @@ def loop():
     
     control_pump(True)  # Turn off the pump
 
-
-class Scale:
-    """
-    Represents all USB devices.
-    """
-
-    def __init__(self, vid: hex, pid: hex) -> None | ValueError:
-        """
-        Locates USB device and initializes attributes.
-        
-        Paramaters:
-            vid: Vendor ID of USB device
-            pid: Product ID of USB device
-        """
-
-        self.dev = usb.core.find(idVendor=vid, idProduct=pid)
-
-        if self.dev is None:
-            raise ValueError('Device not found')
-
-        self.ep = self.dev[0][(0,0)][0]
-        self.ep_address = self.ep.bEndpointAddress
-        self.ep_packet_size = self.ep.wMaxPacketSize
-        
-    def read_weight(self) -> int:
-        """
-        Returns the weight read by the scale.
-        """
-        if self.dev.is_kernel_driver_active(0):
-            self.dev.detach_kernel_driver(0)
-
-        try:
-            data = self.dev.read(self.ep_address, self.ep_packet_size)
-        except:
-            sys.exit("Data not read")
-
-        weight = data[4] + (256 * data[5])
-
-        return weight
-
 def add_to_csv(data: list):
     # current_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     # data = [current_datetime, number]
@@ -124,27 +123,7 @@ def add_to_csv(data: list):
         writer = csv.writer(csvfile)
         writer.writerow(data)
 
-def continous_read():
-    scale = Scale(0x0922, 0x8003)
-    global SCALE_DATA
-    while True:
-        SCALE_DATA = scale.read_weight()
-
-def process_data():
-    while True:
-        if SCALE_DATA is not None:
-            print(f"Weight: {SCALE_DATA} g")
-            add_to_csv(SCALE_DATA)
-
-        time.sleep(5)
-
-
 if __name__ == "__main__":
-    
-    data_t = threading.Thread(target=continous_read)
-    data_t.daemon = True
-    data_t.start()
-
     process_t = threading.Thread(target=loop)
     process_t.daemon = True
     process_t.start()
