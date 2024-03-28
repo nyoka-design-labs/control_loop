8bbd95d6b1ad3fc20ff097287b3934d83debf5ec
arulh <arul.howard@telus.net>
2024-03-27 17:21:16 -0700

    created Sensor class

--- src/usb_devices/ham_sensor.py
+++ src/usb_devices/ham_sensor.py
@@ -1,32 +1,48 @@
-# import usb.core
-
-# dev = usb.core.find(idVendor=0x0403, idProduct=0x6001)
-
-# if dev is None:
-#     raise ValueError("USB device not found")
-
-# ep = dev[0][(0,0)][0]
-# # print(ep)
-
-# if dev.is_kernel_driver_active(0):
-#     dev.detach_kernel_driver(0)
-
-# dev.reset()
-# data = dev.read(ep.bEndpointAddress, 10)
-
-# print(data)
-
-# from pymodbus.client.serial import ModbusSerialClient
 import pymodbus.client as ModbusClient
 
-client = ModbusClient.ModbusSerialClient(port='/dev/ttyUSB0', baudrate=19200, stopbits=2, bytesize=8, parity='N')
-client.connect()
+class Sensor:
+    """
+    Class for reading from the Hamilton sensor.
+    """
 
-res = client.read_holding_registers(method='rtu', address=2409, count=10, unit=1)
-# res = client.read_input_registers(0, 1)
+    def __init__(self, type: str):
+        """
+        Initialize the sensor.
+        """
 
-# rr = client.read_coils(1, 1, slave=1)
+        if (type == "do"):
+            self.mode = "do"
+            self.client = ModbusClient.ModbusSerialClient(port='/dev/ttyUSB0', baudrate=19200,
+                                                       stopbits=2, bytesize=8, parity='N')
+        elif (type == "ph"):
+            self.mode = "ph"
+            pass
+        else:
+            raise ValueError("Invalid sensor type")
+        
+        self.client.connect()
 
-print(res)
-print(res.registers)
-print(res.decode)
+    def read(self) -> tuple:
+        """
+        Read from the sensor.
+        """
+
+        res = self.client.read_holding_registers(method='rtu', address=1928, count=4, unit=1)
+        return (res.registers)
+
+    def close(self):
+        self.client.close()
+
+if __name__ == "__main__":
+    sensor = Sensor(type="do")
+    print(sensor.read(2409, 10))
+    sensor.close()
+
+# client = ModbusClient.ModbusSerialClient(port='/dev/ttyUSB0', baudrate=19200, stopbits=2, bytesize=8, parity='N')
+# client.connect()
+
+# res = client.read_holding_registers(method='rtu', address=2409, count=10, unit=1)
+
+# print(res)
+# print(res.registers)
+# print(res.decode)
