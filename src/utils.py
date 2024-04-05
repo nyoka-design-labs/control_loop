5fca85c1300813354ea61f301bdaf69819d903ae
arulh <arul.howard@telus.net>
2024-04-04 18:20:21 -0700

    added expected volume function

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,25 @@
 import csv
+import math
+
+def exponential_func(t: int, c1: int) -> float:
+    """
+    Returns the expected volume at elapsed time t.
+    """
+
+    # target concentration constants
+    x0 = 22 # g/L
+    mu = 0.1 # rate
+
+    # target substrate concentration
+    X_t = lambda t: x0*math.exp(mu*t) # g/L
+
+    # volume constants
+    v1 = 2 # L
+    c2 = 225 # g/L
+
+    v2 = (v1*X_t(t) - v1*c1) / (c2 - X_t(t)) # L
+
+    return v2
 
 def add_to_csv(data: list, csv_path: str):
     with open('test.csv', 'a', newline='') as csvfile:
