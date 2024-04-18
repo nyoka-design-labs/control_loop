93516831726b325a7223a63b12e0128648b1ee7a
Smarth Gupta <102758221+straw-hat-gupta@users.noreply.github.com>
2024-04-18 14:13:03 -0700

    changed frontend layout
    - added side bar with Devices and SystemControlPanel tabs
    - moved buttons on SystemControlPanel page around
    - changed buttons to reflect the correct operation they are performing

---
+++ webapp/frontend/src/views/Devices.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Devices = () => {
+  return (
+    <div>
+      <h2>Devices Management</h2>
+      {/* Additional content here */}
+    </div>
+  );
+};
+
+export default Devices;
