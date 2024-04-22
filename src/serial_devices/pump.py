d5c9d3934a1e0e24335fd657d83290710d80c1c0
Smarth Gupta <102758221+straw-hat-gupta@users.noreply.github.com>
2024-04-22 08:50:04 -0700

    change main to feed

--- src/serial_devices/pump.py
+++ src/serial_devices/pump.py
@@ -8,24 +8,28 @@ class Pump:
 
     def __init__(self, type: str):
 
-        if (type != "main" and type != "ph"):
+        if (type != "feed" and type != "ph"):
             raise ValueError("Invalid pump mode")
 
-        if (type == "main"):
-            self.state = 0 # 0=OFF, 1=ON
-        elif (type == "ph"):
-            self.state = 2 # 2=OFF, 3=ON
+        # feed: 0=OFF, 1=ON ph: 2=OFF, 3=ON
+        self.state = 0 if type == "feed" else 2
 
         self.mode = type
 
     def control(self, turn_on: bool) -> str:
         """
-        Sends command to Arduino to control the pump.
+        Adjusts the pump state based on the desired command to turn on or off.
+        Handles state transitions.
         """
+        if turn_on:
+            if self.state in [0, 2]:  # If the pump is off in either mode, turn it on
+                self.state += 1
+        else:
+            if self.state in [1, 3]:  # If the pump is on in either mode, turn it off
+                self.state -= 1
 
-        command = 1 if turn_on else 0
-        self.state = self.state ^ command # XOR the provided state
-        return str(self.state) # XOR the provided state
+        # Return the new state as a string for any potential debugging/logging
+        return str(self.state)
     
     def toggle(self) -> str:
         """
@@ -36,7 +40,7 @@ class Pump:
 
 if __name__ == "__main__":
     # example usage of Pump class
-    pump = Pump(type="main")
+    pump = Pump(type="feed")
 
     try:
         while True:
