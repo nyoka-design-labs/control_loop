7729e6a7a12b43fb03557853a1740f33527a255f
Smarth Gupta <strawhatgupta@gmail.com>
2024-07-17 15:55:43 -0700

    minor change

--- webapp/frontend/src/components/Graph.js
+++ webapp/frontend/src/components/Graph.js
@@ -4,7 +4,7 @@ import CanvasJSReact from './canvasjs.react'; // Make sure the path is correct
 const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 export const Graph = ({ systemData, label, actualColor, expectedDataKey = null, expectedColor = 'rgb(255, 99, 132)' }) => {
-    const dataPoints = systemData.map(data => ({ x: new Date(data.time), y: data[label.toLowerCase()] }));
+    const dataPoints = systemData.map(data => ({ x: data.time, y: data[label.toLowerCase()] }));
     const expectedDataPoints = expectedDataKey ? systemData.map(data => ({ x: new data.time, y: data[expectedDataKey.toLowerCase()] })) : [];
 
     const options = {
