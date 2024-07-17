094e855b5dd479d8c6518b082b8dd6bed0a7e217
Smarth Gupta <strawhatgupta@gmail.com>
2024-07-16 20:13:07 -0700

    minor changes

--- webapp/frontend/src/views/FermentationControlPanel.js
+++ webapp/frontend/src/views/FermentationControlPanel.js
@@ -32,39 +32,42 @@ const FermentationControlPanel = () => {
                     {controlLoopStatus}
                 </div>
             </div>
-            <Tabs defaultActiveKey="weight" className="mb-3 custom-tabs">
-                <Tab eventKey="weight" title="Weight">
-                    <h3>Feed Weight: {currentMeasurements.weight} g</h3>
-                    <h3>Lactose Weight: {currentMeasurements.expected_weight} g</h3>
-                    <Chart systemData={systemData} label="Feed_Weight" actualColor="rgb(75, 192, 192)"
-                        expectedDataKey="lactose_weight" expectedColor="rgb(255, 99, 132)" />
-                </Tab>
-                <Tab eventKey="do" title="DO">
-                    <h3>DO: {currentMeasurements.do} %</h3>
-                    <Chart systemData={systemData} label="do" color="rgb(75, 192, 192)"
-                        expectedDataKey="do" expectedColor="rgb(75, 192, 192)" />
-                </Tab>
-                <Tab eventKey="temp" title="Temperature">
-                    <h3>Temp: {currentMeasurements.temp} °C</h3>
-                    <Chart systemData={systemData} label="Temperature" color="rgb(75, 192, 192)"
-                        expectedDataKey="Temperature" expectedColor="rgb(75, 192, 192)" />
-                </Tab>
-                <Tab eventKey="ph" title="pH">
-                    <div className="mb-3">
-                        <h3>pH: {currentMeasurements.ph}</h3>
-                    </div>
-                    <Chart systemData={systemData} label="PH" color="rgb(75, 192, 192)"
-                        expectedDataKey="PH" expectedColor="rgb(75, 192, 192)" />
-                </Tab>
-            </Tabs>
+            <div>
 
-            <div className="control-panel">
-                <div className="right-aligned-buttons">
-                    <div className="mb-3">
-                        <DynamicComponents pumps={currentPumps} loopIdentifier={fermentationLoopIdentifier} />
-                    </div>
-                    <div className="config-section">
-                        <DynamicConfigComponent loopIdentifier={fermentationLoopIdentifier} />
+                <Tabs defaultActiveKey="weight" className="mb-3 custom-tabs">
+                    <Tab eventKey="weight" title="Weight">
+                        <h3>Feed Weight: {currentMeasurements.weight} g</h3>
+                        <h3>Lactose Weight: {currentMeasurements.expected_weight} g</h3>
+                        <Chart systemData={systemData} label="Feed_Weight" actualColor="rgb(75, 192, 192)"
+                            expectedDataKey="lactose_weight" expectedColor="rgb(255, 99, 132)" />
+                    </Tab>
+                    <Tab eventKey="do" title="DO">
+                        <h3>DO: {currentMeasurements.do} %</h3>
+                        <Chart systemData={systemData} label="do" color="rgb(75, 192, 192)"
+                            expectedDataKey="do" expectedColor="rgb(75, 192, 192)" />
+                    </Tab>
+                    <Tab eventKey="temp" title="Temperature">
+                        <h3>Temp: {currentMeasurements.temp} °C</h3>
+                        <Chart systemData={systemData} label="Temperature" color="rgb(75, 192, 192)"
+                            expectedDataKey="Temperature" expectedColor="rgb(75, 192, 192)" />
+                    </Tab>
+                    <Tab eventKey="ph" title="pH">
+                        <div className="mb-3">
+                            <h3>pH: {currentMeasurements.ph}</h3>
+                        </div>
+                        <Chart systemData={systemData} label="PH" color="rgb(75, 192, 192)"
+                            expectedDataKey="PH" expectedColor="rgb(75, 192, 192)" />
+                    </Tab>
+                </Tabs>
+
+                <div className="control-panel">
+                    <div className="right-aligned-buttons">
+                        <div className="mb-3">
+                            <DynamicComponents pumps={currentPumps} loopIdentifier={fermentationLoopIdentifier} />
+                        </div>
+                        <div className="config-section">
+                            <DynamicConfigComponent loopIdentifier={fermentationLoopIdentifier} />
+                        </div>
                     </div>
                 </div>
             </div>
